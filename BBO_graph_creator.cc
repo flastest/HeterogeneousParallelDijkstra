48cd6dadf15d8f7cfe64d9e007eb0fa71957c1a5
flastest <pleasantbowel5yndrome@gmail.com>
2020-03-03 23:23:30 -0800

    no compiler errors first try for cit_graph_from_file wassup

--- BBO_graph_creator.cc
+++ BBO_graph_creator.cc
@@ -41,7 +41,10 @@ std::vector<int> get_teammates (std::string team_list)
 // every graph needs a num nodes (can be calculated by keeping track of mmaximum nsew number)
 // needs array of edges
 // needs weights
-// needs num arcs (which is just i at the end * 2)
+
+//try with DotaLeague/DotaLeague_Edge_Basic or
+//my favorite:
+// BBO/BBO_Edge_Basic!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 graph_t graph_from_file(std::string name)
 {
 	std::ifstream infile(name);
@@ -52,53 +55,54 @@ graph_t graph_from_file(std::string name)
 
 	std::vector<int> weight_vec;
 
-	std::string team_a, team_b;
-
 	std::string input;
 
 	while (infile >> input)// >> ", 0," >>team_a>>", 0,">>team_b>>", 0")
 	{
 
-		std::string team_a = input.substr(input.find(",")+3,input.size());
-		std::string team_b = team_a.substr(team_a.find(",")+3,team_a.size());
-		team_a = team_a.substr(team_a.find(",")+3);
-		team_b = team_b.substr(team_b.find(",")+3);
-
-		//for each teammmate in team_a, we need to add an edge from that teammate to every member in team_b
-
-		auto team_a_vec = get_teammates(team_a);
-		auto team_b_vec = get_teammates(team_b);
-
-		for(int teammate : team_a_vec)
+		if (input.substr(1) != "#")
 		{
-			for(int opponent : team_b_vec)
-			{
-				edge_vec.push_back(Edge(teammate,opponent));
-				weight_vec.push_back(1);
+			std::string team_a = input.substr(input.find(",")+3,input.size());
+			std::string team_b = team_a.substr(team_a.find(",")+3,team_a.size());
+			team_a = team_a.substr(team_a.find(",")+3);
+			team_b = team_b.substr(team_b.find(",")+3);
 
-			}
-			if (teammate > biggest_player_id) 
-			{
-				biggest_player_id = teammate;
-			}
-		}
+			//for each teammmate in team_a, we need to add an edge from that teammate to every member in team_b
 
-		for(int opponent : team_b_vec)
-		{
-			//infclude the following for bidirectional graph
-			/*
+			auto team_a_vec = get_teammates(team_a);
+			auto team_b_vec = get_teammates(team_b);
+
 			for(int teammate : team_a_vec)
 			{
-				edge_vec.push_back(Edge(opponent,teammate));
-				weight_vec.push_back(1);
+				for(int opponent : team_b_vec)
+				{
+					edge_vec.push_back(Edge(teammate,opponent));
+					weight_vec.push_back(1);
 
+				}
+				if (teammate > biggest_player_id) 
+				{
+					biggest_player_id = teammate;
+				}
 			}
-			*/
-			
-			//to keep track of how many nodes we have
-			if (opponent > biggest_player_id) 
+
+			for(int opponent : team_b_vec)
 			{
-				biggest_player_id = opponent;
+				//infclude the following for bidirectional graph
+				/*
+				for(int teammate : team_a_vec)
+				{
+					edge_vec.push_back(Edge(opponent,teammate));
+					weight_vec.push_back(1);
+
+				}
+				*/
+				
+				//to keep track of how many nodes we have
+				if (opponent > biggest_player_id) 
+				{
+					biggest_player_id = opponent;
+				}
 			}
 		}
 
@@ -128,5 +132,70 @@ graph_t graph_from_file(std::string name)
 	graph_t g(edge_array, edge_array + num_arcs, weights, num_nodes);
 
 	return g;
+
+}
+
+
+graph_t cit_graph_from_file(std::string name)
+{
+	//if a line starts with #, ignore it.
+	std::ifstream infile(name);
+
+	int biggest_player_id = 0;
+
+	std::vector<Edge> edge_vec;
+
+	std::vector<int> weight_vec;
+
+	std::string node_a, node_b;
+
+	std::string input;
+
+	while (infile >> input)// >> ", 0," >>team_a>>", 0,">>team_b>>", 0")
+	{
+
+		if (input.substr(1) != "#")
+		{
+			std::string node_a = input.substr(input.find(" "));
+			std::string node_b = input.substr(input.find(" ") +1,input.size());
+			
+			//for each teammmate in team_a, we need to add an edge from that teammate to every member in team_b
+
+			int start = std::stoi(node_a);
+			int end = std::stoi(node_b);
+
+			weight_vec.push_back(1);
+			edge_vec.push_back(Edge(start,end));
+
+
+		}
+
+	}
+
+
+	//evidence that IDK how to use arrays
+	Edge edge_array[edge_vec.size()]; 
+
+	for (uint32_t i = 0; i < edge_vec.size(); i++)
+	{
+		edge_array[i] = edge_vec[i];
+	}
+
+	int weights[weight_vec.size()]; 
+
+	for (uint32_t i = 0; i < weight_vec.size(); i++)
+	{
+		weights[i] = 1;
+	}	
+
+
+	int num_arcs = sizeof(edge_array) / sizeof(Edge);
+
+	int num_nodes = biggest_player_id;
+
+	graph_t g(edge_array, edge_array + num_arcs, weights, num_nodes);
+
+	return g;
+
 
 }
