cd53856d0d492328240d106b44c432db2e52964e
flastest <pleasantbowel5yndrome@gmail.com>
2020-03-22 23:41:35 -0700

    added some comments

--- graph_gen.hh
+++ graph_gen.hh
@@ -14,11 +14,14 @@ void generate_graph(std::string file_name, int number_of_nodes, int number_of_ed
 	ofstream file_stream;
   	file_stream.open (file_name);
 
+  	file_stream << "# a graph with "<< number_of_nodes << " nodes and " << number_of_edges << " edges.\n";
+  	file_stream << "# in general, I saw it was good to have 5 times as many edges as nodes to get a fully connected graph.\n";
 
 
-  	for (int i = 0; i < number_of_edges; i++)
+  	for (int i = 1; i <= number_of_edges; i++)
   	{
-  		file_stream << rand() % number_of_nodes << " " << rand() % number_of_nodes;
+  		file_stream << 	to_string(rand() % number_of_nodes) << " " << to_string(rand() % number_of_nodes);
+  		if (i < number_of_edges) file_stream << "\n";
   	}
 
   	file_stream.close();
