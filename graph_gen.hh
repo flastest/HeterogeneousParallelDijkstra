e46bb4ee4bf0d550c33de6490a4268dd41809918
flastest <pleasantbowel5yndrome@gmail.com>
2020-04-01 16:23:41 -0700

    removed ternary operator

--- graph_gen.hh
+++ graph_gen.hh
@@ -21,7 +21,15 @@ void generate_graph(std::string file_name, int number_of_nodes, int number_of_ed
 	for (int i = 1; i <= number_of_edges; i++)
 	{
     int point_a = rand() % number_of_nodes;
-    int point_b = rand() % number_of_nodes == point_a ? (point_b + 1) % number_of_nodes : point_b; 
+    int point_b = (rand() % number_of_nodes);
+    //std::cout<<"point_a is "<<point_a<<std::endl;
+    if (point_b == point_a)
+    {
+    //    std::cout<<"point_b is "<<point_b<<std::endl;
+        point_b =(point_b + 1) % number_of_nodes;
+    //    std::cout<<"point_b is changed to "<<point_b<<std::endl;
+
+    } 
 		file_stream << 	to_string(point_a) << " " << to_string(point_b);
 		if (i < number_of_edges) file_stream << "\n";
 	}
