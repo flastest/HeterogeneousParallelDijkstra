defe1c1e7f0f0f3ce248ce62cbee94e4e6e424e9
flastest <pleasantbowel5yndrome@gmail.com>
2020-04-01 10:00:37 -0700

    graph_gen should work

--- graph_gen.hh
+++ graph_gen.hh
@@ -12,18 +12,18 @@ void generate_graph(std::string file_name, int number_of_nodes, int number_of_ed
 {
 	srand(time(0));
 	ofstream file_stream;
-  	file_stream.open (file_name);
+	file_stream.open (file_name);
 
-  	file_stream << "# a graph with "<< number_of_nodes << " nodes and " << number_of_edges << " edges.\n";
-  	file_stream << "# in general, I saw it was good to have 5 times as many edges as nodes to get a fully connected graph.\n";
+	file_stream << "# a graph with "<< number_of_nodes << " nodes and " << number_of_edges << " edges.\n";
+	file_stream << "# in general, I saw it was good to have 5 times as many edges as nodes to get a fully connected graph.\n";
 
 
-  	for (int i = 1; i <= number_of_edges; i++)
-  	{
-  		file_stream << 	to_string(rand() % number_of_nodes) << " " << to_string(rand() % number_of_nodes);
-  		if (i < number_of_edges) file_stream << "\n";
-  	}
+	for (int i = 1; i <= number_of_edges; i++)
+	{
+		file_stream << 	to_string(rand() % number_of_nodes) << " " << to_string(rand() % number_of_nodes);
+		if (i < number_of_edges) file_stream << "\n";
+	}
 
-  	file_stream.close();
+	file_stream.close();
 }
 
